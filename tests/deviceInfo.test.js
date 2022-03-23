e9773d9a5b9a334e6b86e1518952fcd6160496d1
danyo <danyo@example.com>
2022-03-24 08:57:22 +1300

    test(deviceinfo): Add a test for iterator.return

--- tests/deviceInfo.test.js
+++ tests/deviceInfo.test.js
@@ -29,14 +29,28 @@ describe('Device info tests', function () {
     expect(backup.isOnline).toEqual(true)
   })
 
-  it('continues iterating new values after initial refresh', async function() {
+  it('continues iterating new values after initial refresh', async function () {
     await testUtils.createDevicesAsync(env)
     const deviceInfoList = []
 
     testUtils.convertIteratorToCallback(deviceInfo.iterator(), (device) => deviceInfoList.push(device))
-    await testUtils.sleep(1000)
+    await testUtils.sleep(500)
     await deviceInfo.refresh()
 
     expect(deviceInfoList).toHaveLength(4)
+  })
+
+  it('stops emitting new values after return called', async function () {
+    await testUtils.createDevicesAsync(env)
+    const deviceInfoList = []
+    const iterator = deviceInfo.iterator()
+    testUtils.convertIteratorToCallback(iterator, (device) => deviceInfoList.push(device))
+    await testUtils.sleep(500)
+    const returnValue = await iterator.return()
+    await deviceInfo.refresh()
+    await testUtils.sleep(500)
+
+    expect(deviceInfoList).toHaveLength(2)
+    expect(returnValue.done).toBe(true)
   })
 })
