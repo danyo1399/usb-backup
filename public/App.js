f8002eafea62c6df0808a8134bf1e9349c5e21d2
danyo1399 <danyo1399@nomail.com>
2022-04-12 01:53:47 +1200

    fix: website broken due to file rename

--- public/App.js
+++ public/App.js
@@ -8,7 +8,7 @@ import JobLog from './features/Jobs/components/JobLog.js'
 import LicenceDialog from './components/Licence.js'
 
 import * as globals from './globals.js'
-import Test from './Test.js'
+import Test from './TestPage.js'
 import ViewFiles from './features/Files/pages/ViewFiles.js'
 import Files from './pages/Files.js'
 import { useOnFocus } from './hooks.js'
