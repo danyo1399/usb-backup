63729583365ed913f5af28d0aab90f956bd975db
danyo1399 <danyo1399@nomail.com>
2022-04-26 00:23:21 +1200

    fix: update stats on failure

--- src/jobs/restoreBackupFilesToSourceRequest.js
+++ src/jobs/restoreBackupFilesToSourceRequest.js
@@ -20,35 +20,38 @@ exports.createRestoreBackupFilesToSourceRequest = async (backupDeviceId, sourceD
     const backupDevice = await assertDeviceOnlineAsync(backupDeviceId, { deviceType: 'backup' })
     const sourceDevice = await assertDeviceOnlineAsync(sourceDeviceId, { deviceType: 'source' })
 
-    // remove any leading slashes
-    sourcePaths = sourcePaths.map(x => x.replaceAll('\\', '/')).map(p => p.startsWith('/') ? p.substr(1) : p)
-    for (const path of sourcePaths) {
-      if (_abort) break
-      const files = await findFilesByPathAsync(backupDeviceId, path)
-      for (const file of files) {
+    try {
+          // remove any leading slashes
+      sourcePaths = sourcePaths.map(x => x.replaceAll('\\', '/')).map(p => p.startsWith('/') ? p.substr(1) : p)
+      for (const path of sourcePaths) {
         if (_abort) break
-        const sourcePath = joinPaths(backupDevice.path, file.relativePath)
-        const existingFile = (await getFilesByHashAndDeviceTypeAsync(file.hash, 'source'))[0]
-        const targetPath = _getTargetPath(path, file.relativePath, copyToRelativePath, sourceDevice.path)
-        const sourceFileExists = await fs.pathExists(sourcePath)
-        if (!sourceFileExists) {
-          log.error(`backup file does not exist on device ${sourcePath}`)
-          continue
+        const files = await findFilesByPathAsync(backupDeviceId, path)
+        for (const file of files) {
+          if (_abort) break
+          const sourcePath = joinPaths(backupDevice.path, file.relativePath)
+          const existingFile = (await getFilesByHashAndDeviceTypeAsync(file.hash, 'source'))[0]
+          const targetPath = _getTargetPath(path, file.relativePath, copyToRelativePath, sourceDevice.path)
+          const sourceFileExists = await fs.pathExists(sourcePath)
+          if (!sourceFileExists) {
+            log.error(`backup file does not exist on device ${sourcePath}`)
+            continue
+          }
+          if (existingFile) {
+            log.warn(`Skipping file as another file with the same hash exists on a source device. ${sourcePath}, ${file.hash}`)
+            continue
+          }
+          const { hash, path: newPath } = await copyFileAsync(sourcePath, targetPath, { overwrite: true })
+          log.info(`copied file from ${sourcePath} to ${newPath}`)
+          await createFileAsync(sourceDevice, newPath, { hash: hash })
         }
-        if (existingFile) {
-          log.warn(`Skipping file as another file with the same hash exists on a source device. ${sourcePath}, ${file.hash}`)
-          continue
-        }
-        const { hash, path: newPath } = await copyFileAsync(sourcePath, targetPath, { overwrite: true })
-        log.info(`copied file from ${sourcePath} to ${newPath}`)
-        await createFileAsync(sourceDevice, newPath, { hash: hash })
       }
+    } finally {
+      log.debug('Updating device stats')
+      await updateDeviceStatsAsync()
+
+      log.debug('writing device meta file')
+      await writeDeviceMetaFileAsync(sourceDevice)
     }
-
-    log.debug('Updating device stats')
-    await updateDeviceStatsAsync()
-
-    await writeDeviceMetaFileAsync(sourceDevice)
   }
 
   async function abort () {
